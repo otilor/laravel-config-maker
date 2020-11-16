fb4fce8700cc11ea139a8ec175a7b969f42811a4
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-11-16 22:51:35 +0100

    Copy the contents of the slug to the config file

--- src/LaravelConfigMaker.php
+++ src/LaravelConfigMaker.php
@@ -2,12 +2,16 @@
 
 namespace Gabrielfemi\LaravelConfigMaker;
 
+use Illuminate\Filesystem\Filesystem;
+
 class LaravelConfigMaker
 {
     public static function create($fileName) : bool
     {
-        $configDir =  'config/';
-        file_put_contents($configDir . $fileName, '');
+        $configDir = 'config/';
+        (new Filesystem())->ensureDirectoryExists(app_path($configDir));
+        file_put_contents('config/' . $fileName,"");
+        copy(__DIR__ . '/Slugs/config.php',  __DIR__ . '/../config/' . $fileName);
 
         return true;
     }
