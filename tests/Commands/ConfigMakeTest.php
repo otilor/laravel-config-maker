6b1113bc47973acf351d9b0a08103626f387922e
GabrielFemi <GabrielFemi@users.noreply.github.com>
2020-11-16 22:23:32 +0000

    Fix styling

--- tests/Commands/ConfigMakeTest.php
+++ tests/Commands/ConfigMakeTest.php
@@ -3,8 +3,6 @@
 
 namespace Gabrielfemi\LaravelConfigMaker\Tests\Commands;
 
-
-
 use Faker\Factory;
 use Gabrielfemi\LaravelConfigMaker\Tests\TestCase;
 
@@ -13,6 +11,6 @@ class ConfigMakeTest extends TestCase
     /** @test */
     public function it_can_run_the_config_make_command()
     {
-       $this->artisan('config:make', ['file' => Factory::create()->slug(2)])->assertExitCode(0);
+        $this->artisan('config:make', ['file' => Factory::create()->slug(2)])->assertExitCode(0);
     }
 }
