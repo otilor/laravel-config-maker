b97103d9589a88fabb67afdd3a0c04e4b5da3106
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-11-16 07:38:15 +0100

    Add create method

--- src/LaravelConfigMaker.php
+++ src/LaravelConfigMaker.php
@@ -4,4 +4,11 @@ namespace Gabrielfemi\LaravelConfigMaker;
 
 class LaravelConfigMaker
 {
+    public static function create($fileName) : bool
+    {
+        $configDir =  'config/';
+        file_put_contents($configDir . $fileName, '');
+
+        return true;
+    }
 }
