556e4fd12cc423c70712be70bfa6ca861aea5951
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-11-16 23:19:40 +0100

    Add test for config:make command

---
+++ tests/Commands/ConfigMakeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Gabrielfemi\LaravelConfigMaker\Tests\Commands;
+
+
+
+use Faker\Factory;
+use Gabrielfemi\LaravelConfigMaker\Tests\TestCase;
+
+class ConfigMakeTest extends TestCase
+{
+    /** @test */
+    public function it_can_run_the_config_make_command()
+    {
+       $this->artisan('config:make', ['file' => Factory::create()->slug(2)])->assertExitCode(0);
+    }
+}
